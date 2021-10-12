845c6f682c6dbbb9aa11506af9cfef51822ee4ed
arnaudhugo <arnaud_h@etna-alternance.net>
2021-10-12 20:37:36 +0200

    [ADD] sso connect try

--- back/node/controllers/sso.js
+++ back/node/controllers/sso.js
@@ -1,31 +1,27 @@
 'use strict';
 
-const fetch              = require('node-fetch');
-const router    = require('express').Router();
-const auth                  = require('../middleware/auth.js');
-const TokenGenerator     = require('uuid-token-generator');
-const TinyDB             = require('tinydb');
-// import fetch from 'node-fetch'
-// import TokenGenerator from 'uuid-token-generator'
-// import TinyDB from 'tinydb'
+const fetch             = require('node-fetch');
+const router            = require('express').Router();
+const auth              = require('../middleware/auth.js');
+const TokenGenerator    = require('uuid-token-generator');
+const TinyDB            = require('tinydb');
 
-const test_db = new TinyDB('./test.db');
+const test_db       = new TinyDB('./test.db');
 
-const sso_front = 'http://135.125.203.6:8079';
-const sso_back = 'http://135.125.203.6:8083';
-
-const apitoken = "c9a94ee6579145b7b9b5c7dbfff70ab8";
-const registry_id = "5a5d6b6a-9879-48ac-8127-a998e4bc88ca";
+const sso_front     = 'http://135.125.203.6:8079';
+const sso_back      = 'http://135.125.203.6:8083';
+const apitoken      = "c9a94ee6579145b7b9b5c7dbfff70ab8";
+const registry_id   = "5a5d6b6a-9879-48ac-8127-a998e4bc88ca";
 
 router.get('/', async (req, res) => {
     test_db.onReady = async function() {
         const body = {
-            "apitoken": apitoken,
-            "asked": ['id', 'username', 'email'],
-            "valid_until": 180
+            "apitoken":     apitoken,
+            "asked":        ['id', 'username', 'email'],
+            "valid_until":  180
         }
         
-        let response =  await fetch(
+        let response = await fetch(
             `${sso_back}/extern/key`, 
             {
                 method: 'post',
@@ -52,6 +48,39 @@ router.get('/', async (req, res) => {
         console.log(response, ret)
     
         res.status(200).json({ code: 200, data: ret, message: "" });
+    }
+});
+
+router.get('/:uuid', async (req, res) => {
+    const uuid = req.params.id;
+
+    test_db.onReady = async function() {
+        test_db.getInfo(uuid, function(err, key, value) {
+            if (err) {
+                res.status(500).json({ code: 500, data: null, message: err });
+            }
+            
+            console.log('[setInfo] ' + key + ' : ' + value);
+
+            const body = {
+                "apitoken":     apitoken,
+                "secret":       value.secret
+            }
+            
+            let response = await fetch(
+                `${sso_back}/extern/key/${key}/token`, 
+                {
+                    method: 'post',
+                    body:    JSON.stringify(body),
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            )
+            response = await response.json();
+
+            console.log(response)
+            
+            res.status(200).json({ code: 200, data: response.data, message: "" });
+        });
     }
 });
 
