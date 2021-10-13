0f14c477c53f2d70a4b0eee68562efec2f179069
arnaudhugo <arnaud_h@etna-alternance.net>
2021-10-13 08:57:42 +0200

    [FIX] project

--- back/node/controllers/project.js
+++ back/node/controllers/project.js
@@ -109,6 +109,7 @@ router.get('/:id', auth.user(), async (req, res) => {
                 .run(req._rdb)
                 .then(cursor => cursor.toArray())
                 .then(invests => {
+                    console.log(invests)
                     let list = []
                     let totalAmount = 0;
                     let totalValid = 0;
