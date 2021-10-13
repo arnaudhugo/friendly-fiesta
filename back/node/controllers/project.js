a0f654f9df51e5b18b386621bfa4233f3e0df889
arnaudhugo <arnaud_h@etna-alternance.net>
2021-10-13 08:57:07 +0200

    [FIX] project

--- back/node/controllers/project.js
+++ back/node/controllers/project.js
@@ -105,6 +105,7 @@ router.get('/:id', auth.user(), async (req, res) => {
         .then(result => {
             r.table('invest')
                 .filter({ projectId: id })
+                .eq_join('id', r.table('user')).zip()
                 .run(req._rdb)
                 .then(cursor => cursor.toArray())
                 .then(invests => {
